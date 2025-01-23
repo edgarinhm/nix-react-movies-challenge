c9877fa57b19023ab112f7c20d117e18dc398757
Edgar Mejia <edgarinhm@hotmail.com>
2025-01-23 09:25:19 -0500

    add search box aria label

--- src/common/components/search-box/index.jsx
+++ src/common/components/search-box/index.jsx
@@ -1,14 +1,21 @@
-import { FaSearch } from 'react-icons/fa';
-import './search-box.css'
+import { FaSearch } from "react-icons/fa";
+import "./search-box.css";
 
-const SearchBox = ({ value = '', onChange, placeholder }) => {
-  
+const SearchBox = ({ value = "", onChange, placeholder }) => {
   return (
-    <form className='search-bar'>
-        <span><FaSearch className='flip-icon'/></span>
-        <input type="text" value={value} onChange={(event) => onChange(event.target.value)}  placeholder={placeholder ?? ""} />
+    <form className="search-bar">
+      <span>
+        <FaSearch className="flip-icon" />
+      </span>
+      <input
+        type="text"
+        aria-label="search box"
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+        placeholder={placeholder ?? ""}
+      />
     </form>
-  )
-}
+  );
+};
 
 export default SearchBox;
